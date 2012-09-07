d7320233c776382244a6b1db6ea3e4a1b8ad057a
simo385 <simo385@gmail.com>
2012-09-07 21:00:43 +0200

    aggiornamento chiamata metodo gen random

--- src/org/alldifferent/Grafo.java
+++ src/org/alldifferent/Grafo.java
@@ -3,12 +3,9 @@
  */
 package org.alldifferent;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
+
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
@@ -18,10 +15,7 @@ import org.jgrapht.alg.NeighborIndex;
 import org.jgrapht.generate.CompleteBipartiteGraphGenerator;
 import org.jgrapht.graph.ClassBasedVertexFactory;
 import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.SimpleGraph;
-import org.jgrapht.graph.SimpleWeightedGraph;
-import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.graph.SimpleDirectedGraph;
 import org.jgrapht.traverse.DepthFirstIterator;
 
 /**
@@ -31,7 +25,11 @@ import org.jgrapht.traverse.DepthFirstIterator;
 public class Grafo {
 	
 	private Graph<Object, DefaultEdge> completeGraph;
+	
 	private Vector<Variable> vars;
+	private Set<DefaultEdge> visitedEdge;
+	private String startMMVertex;
+	private String finishMMVertex;
 	/**
 	 * @param args
 	 */
@@ -73,7 +71,7 @@ public class Grafo {
 		vars.add(1, new Variable(2,dX2));
 		vars.add(2, new Variable(3,dX3));
 		*/
-		int domCard = 4;
+		int domCard = 3;
 		CSP c = new CSP();
 		c.generateRandom(domCard);
 		vars = c.getConstraint().getVariables();
@@ -84,6 +82,7 @@ public class Grafo {
 		g.createBipartiteGraph(nValuesVars, nValuesDms, vars);
 		
 	}
+	
 	
 	//rimpiazza un vecchio vertice con uno nuovo mantenendo le connessioni
 	private boolean replaceVertex(Object oldVertex, Object newVertex)
@@ -142,9 +141,11 @@ public class Grafo {
 	//questo metodo crea un grafo bipartito con variabili e valori dei domini
 	public void createBipartiteGraph(int nValuesVars, int nValuesDms, Vector<Variable> variables) {
 		vars = variables;
+		startMMVertex = new String("S");
+		finishMMVertex = new String("F");
 		
 		//Generazione di un grafo bipartito completo
-		completeGraph = new SimpleGraph<Object, DefaultEdge>(DefaultEdge.class);
+		completeGraph = new SimpleDirectedGraph<Object, DefaultEdge>(DefaultEdge.class);
 		CompleteBipartiteGraphGenerator<Object, DefaultEdge> bip = new CompleteBipartiteGraphGenerator<Object, DefaultEdge>(nValuesVars, nValuesDms);
 		//Tipo di vertici che deve creare il generatore
 		VertexFactory<Object> v = new ClassBasedVertexFactory<Object>(Object.class);
@@ -161,6 +162,7 @@ public class Grafo {
 		Integer counter = 0;
 		int i = 0;
 		Object tmp = new Object();
+		
 		//ciclo su tutti i vertici per rimpiazzare i vecchi vertici con i nuovi
 		for (Object vertex : vertices) {
 			
@@ -168,7 +170,6 @@ public class Grafo {
 				replaceVertex(vertex, vars.get(i));
 				tmp = vars.get(i);
 				i++;
-				
 			}
 			else {
 				//prendo i vertici adiacenti a vertex
@@ -177,7 +178,7 @@ public class Grafo {
 				verAdj.addAll(adj.neighborsOf(vertex));
 				
 				//se il vertex ha come adiacenze una variabile, non posso inserirne un'altra
-				//questo perchè ogni arco collega una variabile ad un valore solo nel suo dominio
+				//questo perchÔøΩ ogni arco collega una variabile ad un valore solo nel suo dominio
 				if(!contains(verAdj, tmp) && i != vars.size()) {
 					replaceVertex(vertex, vars.get(i));
 					//aggiorno tmp con la variabile appena inserita
@@ -190,33 +191,74 @@ public class Grafo {
 				
 			}
 			
-			
 		}
+		//Fino a qui ho creato un grafo bipartito COMPLETO
+		
+		System.out.println(vars.get(0).getDomain().getValues());
+		System.out.println(vars.get(1).getDomain().getValues());
+		System.out.println(vars.get(2).getDomain().getValues());
 		
 		//Sistemo il grafo con i domini delle variabili
 		Set<DefaultEdge> temp = new HashSet<DefaultEdge>();
 		Set<DefaultEdge> allEdge = completeGraph.edgeSet();
-		/*
-		//ciclo su ogni arco e controllo se il valore target è contenuto nel dominio della variabile
+		
+		//ciclo su ogni arco e controllo se il valore target ÔøΩ contenuto nel dominio della variabile
 		//altrimenti rimuovo l'arco dal grafo
 		for(DefaultEdge e : allEdge) {
-			Variable source = (Variable) completeGraph.getEdgeSource(e);
-			Integer target = (Integer) completeGraph.getEdgeTarget(e);
-			System.out.println("Valore nel dominio: " + source.getDomain().getValues());
-			System.out.println("Valore target: " + target.intValue());
-			if(!source.getDomain().getValues().contains(target)) {
-				temp.add(e);
+			
+			//controllo gli archi la cui sorgente è una variabile
+			if(completeGraph.getEdgeSource(e).getClass() == Variable.class) {
+				Variable source = (Variable) completeGraph.getEdgeSource(e);
+				Integer target = (Integer) completeGraph.getEdgeTarget(e);
+				//System.out.println("Valore nel dominio: " + source.getDomain().getValues());
+				//System.out.println("Valore target: " + target.intValue());
+				if(!source.getDomain().getValues().contains(target)) {
+					//aggiungo l'arco nel vettore delle rimozioni
+					temp.add(e);
+				}
+			}
+			else {
+				//controllo gli archi la cui sorgente è un valore del dominio
+				Integer source = (Integer) completeGraph.getEdgeSource(e);
+				Variable target = (Variable) completeGraph.getEdgeTarget(e);
+				//System.out.println("Valore nel dominio: " + target.getDomain().getValues());
+				//System.out.println("Valore target: " + source.intValue());
+				if(!target.getDomain().getValues().contains(source)) {
+					//aggiungo l'arco nel vettore delle rimozioni
+					temp.add(e);
+				}
 			}
 			
 		}
 		
 		completeGraph.removeAllEdges(temp);
-		*/
+		//ho creato il grafo secondo i domini delle variabili
+		
+		//mi prendo tutti i vertici del grafo
+		vertices = new HashSet<Object>();
+		vertices.addAll(completeGraph.vertexSet());
+		
+		//aggiungo i vertici source e finish al grafo
+		completeGraph.addVertex(startMMVertex);
+		completeGraph.addVertex(finishMMVertex);
+		
+		for(Object vertex : vertices) {
+			//se il vertice è una variabile
+			if(vertex.getClass() == Variable.class){
+				completeGraph.addEdge(startMMVertex, vertex);
+			}
+			else {
+				completeGraph.addEdge(vertex, finishMMVertex);
+			}
+			
+		}
+		
 		printGraph();
 
 		System.out.println(completeGraph.toString());		
 	}
 	
+	//metodo che stampa il grafo
 	public void printGraph() {
 		
 		//Stampo la struttura del grafo
